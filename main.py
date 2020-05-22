7dff0823b993b9faf5dfa0f587efca7c2c23849f
Hálisson <tyl3rsec@gmail.com>
2020-05-22 12:11:07 -0300

    Fixed remaining time and cleaned the code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-# Imports
+#Imports
 from argparse import ArgumentParser, FileType, HelpFormatter
 from sys import argv, exit, stdout, stderr
-from requests import get, post, put, delete, head, options, codes, exceptions
-from time import sleep, strftime, gmtime
+from requests import Request, Session, codes, exceptions
+from datetime import timedelta
 import os.path as path_parse
+from socket import socket, AF_INET, SOCK_STREAM
+from urllib.parse import urlparse
 
 # Override "help" message
 class CustomFormatter(HelpFormatter):
@@ -16,11 +18,10 @@ class CustomFormatter(HelpFormatter):
 # Override error messages
 class ArgumentParser(ArgumentParser):
     def error(self, message):
-        #self.print_usage(stderr)
-        self.exit(2, '\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: {}\n'.format(message))
+        self.exit(2, f'\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: {message}\n')
 
 # Parse arguments
-argparse = ArgumentParser(prog="Dirscover", usage="{} <url> <wordlist>.".format(argv[0]),
+argparse = ArgumentParser(prog="Dirscover", usage=f"{argv[0]} <url> <wordlist>.",
         description="Tool to enumerate website directories",
         formatter_class=CustomFormatter)
 argparse._positionals.title = 'Positional arguments'
@@ -29,81 +30,68 @@ argparse.add_argument("url", help="Url to fuzz", type=str)
 argparse.add_argument("wordlist", help="Wordlist to use", type=FileType('r', encoding='utf-8'))
 argparse.add_argument("--params", help="Optional url parameters", type=dict, dest="params")
 argparse.add_argument("--method", help="HTTP method", type=str, default="GET", dest="method")
-argparse.add_argument("--agent", help="Custom user-agent (fill with \"random\" to randomize)", type=str, default="Dirscover v1.0", dest="agent")
+argparse.add_argument("--agent", help="Custom user-agent string/text file.", type=str, default="Dirscover v1.0", dest="agent")
 argparse.add_argument("--data", help="POST request data", type=dict, dest="data")
 argparse.add_argument("--cookies", help="Request cookies", type=dict, dest="cookies")
 args = argparse.parse_args()
 
 # Variables
-HTTP_METHODS = [ 'GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS' ]
-wordlist = args.wordlist.read().split()
+wordlist = args.wordlist.read()
 index = list()
 
 # Check if informed method is valid
-if not args.method in HTTP_METHODS:
-    exit('\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: invalid method specified (`{}`).'.format(args.method))
-
-# Generate valid "requests" user-agent    
-def customAgent(agent):
-    """if (agent != "random"):
-        return { "User-Agent": agent }
-    else:
-        try:
-            with open("/wordlists/user-agent.txt") as wordlist:
-                return { "User-Agent": choice(wordlist.readlines()) }
-        except:
-            print('\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: couldn\'t find user-agent wordlist (`./wordlists/user-agent.txt`).')
-            exit(1)
-    """
-    return { 'User-Agent': ( lambda:agent, lambda:choice(open(agent).readlines()) )[path.isfile(agent)] }
+if not args.method.upper() in [ 'GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS' ]:
+    exit(f'\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: invalid method specified (`{args.method}`).')
 
 # Print formatted output
-def printStats(request, directory):
-    template = """
-     \r «˹=========================( \033[38:5:29mDirscover \033[38:5:15m)=========================˺»
-     \r   \033[38:5:6m┌[ \033[38:5:3mRequest data:
-     \r   \033[38:5:6m└─┬[ \033[38:5:15mHost: \033[38:5:228m{host}
-     \r   \033[38:5:6m  ├[ \033[38:5:15mHTTP method: \033[38:5:228m{method}
-     \r   \033[38:5:6m  ├[ \033[38:5:15mUser-agent: \033[38:5:228m{agent}
-     \r  \033[38:5:6m   └[ \033[38:5:15mPing: \033[38:5:228m{ping:.1f}\033[38:5:15mms
-     \r   \033[38:5:6m┌[ \033[38:5:3mWordlist data:
-     \r  \033[38:5:6m └─┬[ \033[38:5:15mFile: \033[38:5:228m{filename}
-     \r   \033[38:5:6m  ├[ \033[38:5:15mLoaded: \033[38:5:228m{wc} \033[38:5:15mwords
-     \r     \033[38:5:6m└[ \033[38:5:15mEstimated time: \033[38:5:228m{etime}
-     \r   \033[38:5:6m┌[ \033[38:5:3mStatus:
-     \r \033[38:5:6m  └─┬[ \033[38:5:15mTesting: \033[38:5:228m{testing}
-     \r     \033[38:5:6m└[ \033[38:5:15mRemaining: \033[38:5:228m{remaining} \033[38:5:15mwords
-     \r
-               """.format(host=args.url, method=request.request.method.upper(), agent=request.request.headers['User-Agent'],
-                       ping=request.elapsed.total_seconds()*1000, filename=args.wordlist.name, wc=len(wordlist), 
-                       etime=strftime("%H:%M:%S", gmtime((int(request.elapsed.total_seconds())) * (len(wordlist) - wordlist.index(directory)))),
-                       testing=directory, remaining=len(wordlist) - wordlist.index(directory))
-    length = len(template.split('\n')[1:])
-    stdout.write('\n'*length)
-    stdout.write(u'\033[{}F\033[0J '.format(length))
+def printStats(response):
+    template = (f'\n\r \033[38:5:15m╭─────────────────────> \033[38:5:29mDirscover \033[38:5:15m<─────────────────────╮\n'
+     f'\r   \033[38:5:6m┌[ \033[38:5:3mRequest data:\n'
+     f'\r   \033[38:5:6m└─┬[ \033[38:5:15mHost: \033[38:5:228m{urlparse(response.url).netloc}\n'
+     f'\r   \033[38:5:6m  ├[ \033[38:5:15mRequest method: \033[38:5:228m{args.method}\n'
+     f'\r   \033[38:5:6m  ├[ \033[38:5:15mUser-agent: \033[38:5:228m{response.request.headers["User-Agent"]}\n'
+     f'\r  \033[38:5:6m   └[ \033[38:5:15mPing: \033[38:5:228m{response.elapsed.total_seconds()*1000:.1f}\033[38:5:15mms\n'
+     f'\r   \033[38:5:6m┌[ \033[38:5:3mWordlist data:\n'
+     f'\r  \033[38:5:6m └─┬[ \033[38:5:15mFile: \033[38:5:228m{args.wordlist.name}\n'
+     f'\r   \033[38:5:6m  ├[ \033[38:5:15mLoaded: \033[38:5:228m{len(wordlist)} \033[38:5:15mwords\n'
+     f'\r     \033[38:5:6m└[ \033[38:5:15mEstimated time: \033[38:5:228m{(response.elapsed.total_seconds()*(len(wordlist) - wordlist.index(directory)))/60:.0f} minutes\n'
+     f'\r   \033[38:5:6m┌[ \033[38:5:3mStatus:\n'
+     f'\r \033[38:5:6m  └─┬[ \033[38:5:15mTesting: \033[38:5:228m{urlparse(response.url).path}\n'
+     f'\r     \033[38:5:6m└[ \033[38:5:15mRemaining: \033[38:5:228m{(len(wordlist) - wordlist.index(directory)) - 1} \033[38:5:15mwords\n'
+     f'\r')
+    stdout.write('\n'*len(template.split('\n')[1:]))
+    stdout.write(f'\033[{str(len(template.split(chr(10))[1:]))}F\033[0J ')
     for line in template.split('\n')[1:]:
         stdout.write(line)
         stdout.write(u'\033[1E')
-    if request.status_code == codes.ok:
-        index.append({ 'host': path, 'code':request.status_code })
+    if response.status_code == codes.ok:
+        index.append({ 'host': response.url, 'code':response.status_code })
     for discovered in index:
-        stdout.write("\r» {} (Code: {})\n".format(discovered['host'], discovered['code']))
-    stdout.write(u'\033[' + str(length + len(index)) + 'F')
+        stdout.write(f'\r   [+] {discovered["host"]} (Code: {discovered["code"]})\n')
+    stdout.write(' ╰───────────────────────────────────────────────────────╯')
+    stdout.write(f'\033[{str(++len(template.split(chr(10))[1:]) + len(index))}F')
     stdout.flush()
 
 # Enumerate
 for directory in wordlist:
-    path = '{}/{}'.format(args.url, directory)
     try:
         if path_parse.isfile(args.agent):
             with open(args.agent) as agent_list: user_agent = { 'User-Agent':choice(agent_list.readlines()) }
         else:
             user_agent = { 'User-Agent': args.agent }
-        request = eval("{}(path, params=args.params, headers=user_agent, data=args.data, cookies=args.cookies)".format(args.method.lower()))
+        response = Session().request(
+            method=args.method.upper(), 
+            url=f'{args.url}/{directory}',
+            headers=user_agent,
+            data=args.data,
+            params=args.params,
+            cookies=args.cookies)
     except (exceptions.InvalidSchema, exceptions.MissingSchema):
-        print('\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: invalid url structure (missing schema).'.format(args.url.strip()))
+        print('\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: invalid url structure (missing schema).')
         exit(1)
     except KeyboardInterrupt:
-        stdout.write('\033[{}B\r\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: execution interrupted by user (`<CTRL+C>`).\n'.format(15+len(index)))
+        stdout.write(f'\033[{16+len(index)}B'
+                f'\r ╰───────────────────────────────────────────────────────╯\n'
+                f'\r\033[1;36m❰\033[38:5:9m!\033[1;36m❱ \033[1;97mError\033[0;0m: execution interrupted by user (`<CTRL+C>`).\n')
         exit(1)
-    printStats(request, directory)
+    printStats(response)
